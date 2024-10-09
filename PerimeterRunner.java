6b9bfeded9872c4df3696bec9bce07d3869fb90b
MasterDongDong <dongwon3927@gmail.com>
2024-10-09 15:10:36 -0800

    Add files via upload

---
+++ PerimeterRunner.java
@@ -0,0 +1,49 @@
+import edu.duke.*;
+
+public class PerimeterRunner {
+    public double getPerimeter (Shape s) {
+        // Start with totalPerim = 0
+        double totalPerim = 0.0;
+        // Start wth prevPt = the last point 
+        Point prevPt = s.getLastPoint();
+        // For each point currPt in the shape,
+        for (Point currPt : s.getPoints()) {
+            // Find distance from prevPt point to currPt 
+            double currDist = prevPt.distance(currPt);
+            // Update totalPerim by currDist
+            totalPerim = totalPerim + currDist;
+            // Update prevPt to be currPt
+            prevPt = currPt;
+        }
+        // totalPerim is the answer
+        return totalPerim;
+    }
+    public int getNumPoints(Shape s) {
+        int totalPoints = 0;
+        for (Point currPt : s.getPoints()) {
+            totalPoints += 1;
+        }
+        return totalPoints;
+    }
+    public double getAverageLength(Shape s) {
+        double totalPerim = getPerimeter(s);
+        int numPoints = getNumPoints(s);
+        double AverageLength = totalPerim / numPoints;
+        return AverageLength;
+    }
+    public void testPerimeter () {
+        FileResource fr = new FileResource();
+        Shape s = new Shape(fr);
+        double length = getPerimeter(s);
+        System.out.println("perimeter = " + length);
+        
+        int numPoints = getNumPoints(s);
+        System.out.println(numPoints);
+        double AverageLength =  getAverageLength(s);
+        System.out.println(AverageLength);
+    }
+    public static void main (String[] args) {
+        PerimeterRunner pr = new PerimeterRunner();
+        pr.testPerimeter();
+    }
+}
